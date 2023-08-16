9d638b1b6cddf075d1e5f546b037b77b732679a2
pauloWeslleY <weslleyspfc@13outlook.com>
2023-08-15 23:16:48 -0300

    giovanna

---
+++ main.js
@@ -0,0 +1,7 @@
+document.getElementById('aceitarBtn').addEventListener('click', function () {
+  alert('Great I love you 💕💕💕.')
+})
+
+document.getElementById('recusarBtn').addEventListener('click', function () {
+  alert('Ah, tudo bem. Se precisar de um tempo, estarei aqui.')
+})
